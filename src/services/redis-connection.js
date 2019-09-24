94ff48ce11c46edc85034f65a819ec29b40b4253
Damir <damir89@gmail.com>
2019-09-24 15:30:33 +0200

    feat(redis): add tls option

--- src/services/redis-connection.js
+++ src/services/redis-connection.js
@@ -4,17 +4,28 @@ Promise.promisifyAll(redis.RedisClient.prototype)
 Promise.promisifyAll(redis.Multi.prototype)
 
 export class RedisConnection {
-  constructor (host, port, database, password) {
-    this.host = host || '127.0.0.1'
-    this.port = port || '6379'
-    this.database = database || 0
-    this.password = password || false
+  constructor (host = '127.0.0.1', port = '6379', database = 0, password = false, tls = false) {
+    this.host = host
+    this.port = port
+    this.database = database
+    this.password = password
+    this.tls = tls
   }
 
   connect () {
     return Promise.try(() => {
-      const opts = {host: this.host, port: this.port}
-      if (this.password) opts.password = this.password
+      const opts = {
+        host: this.host,
+        port: this.port
+      }
+
+      if (this.password) {
+        opts.password = this.password
+      }
+      if (this.tls) {
+        opts.tls = { checkServerIdentity: () => undefined }
+      }
+
       this.client = redis.createClient(opts)
       if (!this.database) {
         return this.client
